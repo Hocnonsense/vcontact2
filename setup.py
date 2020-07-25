9472acfbd44ac38e87ea7242faa396f692a52662
Ben Bolduc <bolduc.10@osu.edu>
2020-07-25 01:24:50 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='vcontact2',
-      version='0.9.18',
+      version='0.9.19',
       description='Viral Contig Automatic Clutering and Taxonomy',
       url='https://bitbucket.org/MAVERIClab/vcontact2',
       author='Benjamin Bolduc',
